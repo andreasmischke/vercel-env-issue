fe562889c640c4609e14ceeaa585e520ea0f2df3
Andreas Mischke <andreas.mischke@prym.com>
2022-04-05 12:50:16 +0200

    add debug output on the start page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    storybookUrl: process.env.STORYBOOK_URL ?? '<fallback value in next.config.js>',
+    nextAuthUrl: process.env.NEXTAUTH_URL ?? '<fallback value in next.config.js>',
+  }
 }
 
 module.exports = nextConfig
